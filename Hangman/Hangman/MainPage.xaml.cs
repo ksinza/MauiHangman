449fe15dcaf3571248e0419395668591b7bdc61d
kevin sinza <sbc01@SBC01.local>
2023-03-25 15:15:36 -0500

    task: done

--- Hangman/Hangman/MainPage.xaml.cs
+++ Hangman/Hangman/MainPage.xaml.cs
@@ -17,6 +17,51 @@ public partial class MainPage : ContentPage, INotifyPropertyChanged
         }
 
     }
+
+    public string Message {
+        get => message;
+        set {
+
+            message = value;
+            OnPropertyChanged();
+        }
+
+
+    }
+
+    int mistakes = 0;
+    int maxWrong = 6;
+
+
+    private string message { get; set; }
+
+    private string gameStatus { get; set; }
+
+    public string GameStatus
+    {
+        get => gameStatus;
+        set {
+
+            gameStatus = value;
+            OnPropertyChanged();
+        }
+    }
+
+
+    private string currentImage = "img0.jpg";
+
+    public string CurrentImage 
+    {
+        get => currentImage;
+        set
+        {
+
+            currentImage = value;
+            OnPropertyChanged();
+        }
+    }
+
+
     #endregion
 
     #region Fields
@@ -42,24 +87,112 @@ public partial class MainPage : ContentPage, INotifyPropertyChanged
     string answer = "";
     private string spotlight;
 
+    private List<char> letters = new List<char>();
+
     List<char> guessed = new List<char>();
+
+    public List<char> Letters {
+
+        get => letters;
+        set
+        {
+            letters = value;
+            OnPropertyChanged();
+        }
+    }
 
     #endregion
 
     public MainPage()
 	{
 		InitializeComponent();
+        Letters.AddRange("abcdefghijklmnopqrstuvwxyz");
+
         BindingContext = this;
 
         PickWord();
         CalculateWord(answer, guessed);
+
+
+
     }
 
 	private void OnCounterClicked(object sender, EventArgs e)
 	{
-		
+
+        var btn = sender as Button;
+
+        if (btn != null) {
+
+            var letter = btn.Text;
+            btn.IsEnabled = false;
+            HandleGuess(letter[0]);
+        }
+
 	}
 
+    private void HandleGuess(char letter)
+    {
+        if (guessed.IndexOf(letter) == -1) {
+
+            guessed.Add(letter);
+
+        }
+
+        if (answer.IndexOf(letter) >= 0)
+        {
+
+            CalculateWord(answer, guessed);
+            CheckIfGameWon();
+        }
+        else if (answer.IndexOf(letter) == -1)
+        {
+            mistakes++;
+            UpdateStatus();
+            CheckIfGameLost();
+            CurrentImage = $"img{mistakes}.jpg";
+        }
+
+    }
+
+    private void CheckIfGameLost()
+    {
+        if (mistakes == maxWrong) {
+            Message = "You Lost!";
+            DisableLetters();
+        }
+    }
+
+    private void DisableLetters()
+    {
+        foreach (var children in LettersContainer.Children)
+        {
+            var btn = children as Button;
+
+            if (btn != null) {
+
+                btn.IsEnabled = false;
+            }
+        }
+    }
+
+    private void CheckIfGameWon()
+    {
+        if (Spotlight.Replace(" ", "") == answer)
+        {
+
+            Message = "You win!";
+            DisableLetters();
+
+        }
+
+    }
+
+    private void UpdateStatus() {
+
+        GameStatus =  $"Errors: {mistakes} of {maxWrong}";
+
+    }
     #region Game Engine
 
     private void PickWord() {
@@ -80,6 +213,32 @@ public partial class MainPage : ContentPage, INotifyPropertyChanged
 
     }
 
+  
+    void Button_Clicked_1(System.Object sender, System.EventArgs e)
+    {
+        mistakes = 0;
+        guessed = new List<char>();
+        currentImage = "img0.jpg";
+        PickWord();
+        CalculateWord(answer, guessed);
+        Message = "";
+        UpdateStatus();
+        EnableLeLetters();
+    }
+
+    private void EnableLeLetters()
+    {
+        foreach (var children in LettersContainer.Children)
+        {
+            var btn = children as Button;
+
+            if (btn != null)
+            {
+
+                btn.IsEnabled = true;
+            }
+        }
+    }
     #endregion
 }
 
